0ff4364883da59cf1629355349c200467c337ad9
ankita-03 <ankita@gautam.us>
2021-12-14 02:29:03 -0600

    ...

--- app.py
+++ app.py
@@ -2,4 +2,20 @@ from flask import Flask, app, render_template, redirect, url_for
 from flask_pymongo import Pymongo
 import scrape_mars
 
-app = Flask()
+app = Flask(__name__)
+mongo = Pymongo(app, url = "mongodb://localhost:27017/mars_app")
+
+@app.route("/")
+def index(): 
+    dataset = mongo.db.dataset.find_one()
+    return render_template("index.html", dataset=dataset)
+
+@app.route("/scrape")
+def scrape():
+    dataset = mongo.db.dataset
+    mars = scrape_mars.scrape_start()
+    dataset.update({}, mars, upsert = True)
+    return redirect("/")
+
+if __name__== "__main__" :
+    app.run()
